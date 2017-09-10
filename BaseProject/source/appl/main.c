e3a82569d706b0ac4315205567fcc096291399a9
Samuel <samymtz66@gmail.com>
2017-09-09 19:21:37 -0500

    Create timer0.c and timer0.h files in bsw\ecual directory. Move timer0 implementation to these files.

--- BaseProject/source/appl/main.c
+++ BaseProject/source/appl/main.c
@@ -4,6 +4,7 @@
 
 #include "board.h"
 #include "app_scheduler.h"
+#include "timer0.h"
 #include "Tasks.h"    
 #include <stdbool.h>
 #include <stdio.h>
@@ -25,20 +26,6 @@ TaskType Tasks[]={
 /*----------------------------------------------------------------------------
  *        Local functions
  *----------------------------------------------------------------------------*/
-uint8_t timer0_interrupt = 0;
-
-/**
- *  Interrupt handler for TC0 interrupt.
- */
-void TC0_Handler(void)
-{
-    volatile uint32_t dummy;
-    /* Clear status bit to acknowledge interrupt */
-    dummy = TC0->TC_CHANNEL[ 0 ].TC_SR;
-    timer0_interrupt = 1;
-    NVIC_DisableIRQ(TC0_IRQn);
-
-}
 
 /**
  *  \brief Configure LEDs
@@ -49,35 +36,6 @@ static void _ConfigureLeds( void )
 {
 	LED_Configure( 0 ) ;
 	LED_Configure( 1 ) ;
-}
-
-
-/**
- *  Configure Timer Counter 0 to generate an interrupt every 1s.
- */
-static void _ConfigureTc(void)
-{
-    uint32_t div;
-    uint32_t tcclks;
-
-    /** Enable peripheral clock. */
-    PMC_EnablePeripheral(ID_TC0);
-
-    /** Configure TC for a 1Hz frequency and trigger on RC compare. */
-    TC_FindMckDivisor(1, BOARD_MCK/2, &div, &tcclks, BOARD_MCK);
-
-    TC_Configure(TC0, 0, tcclks | TC_CMR_CPCTRG);
-    TC0->TC_CHANNEL[0].TC_RC = (BOARD_MCK / div);
-
-    /* Configure and enable interrupt on RC compare */
-    NVIC_ClearPendingIRQ(TC0_IRQn);
-    NVIC_EnableIRQ(TC0_IRQn);
-
-    NVIC_SetPriority(TC0_IRQn ,2);
-    TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
-
-    TC_Start(TC0, 0);
-
 }
 
 /*----------------------------------------------------------------------------
@@ -122,7 +80,7 @@ extern int main( void )
 		if(timer0_interrupt == 1)
 		{
 		    timer0_interrupt = 0;
-		    printf("Timer Interrupt.\n\r");
+		    printf("Timer0 Interrupt.\n\r");
 		    LED_Toggle( 1 );
 		}
 	}
